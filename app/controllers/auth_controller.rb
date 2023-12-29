4d232d70c006697dcca0d1589518d2c6fe73383f
peshk1n <anna_konoplyanova@mail.ru>
2023-12-29 11:50:58 +0300

    Add auth

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,4 +3,15 @@ class AuthController < ApplicationController
         user = User.new
         #redirect_to '/'
     end
+
+    def create
+        user_params = params.require(:session)
+        user = User.find_by(login: user_params[:login])
+
+        if user.present?
+            session[:user_id] = user.id
+            redirect_to root_path
+        end
+
+    end
 end
--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,8 +1,12 @@
 class RegistrationController < ApplicationController
-
     def new
-        user = User.new
-        #redirect_to '/'
+        @user = User.new
+    end
+
+    def create
+        user_params = params.require(:user).permit(:login, :nickname, :password, :code_word)
+        User.create(user_params)
+        redirect_to '/', notice: 'Вы успешно зарегистрировались!'
     end
 
 end
